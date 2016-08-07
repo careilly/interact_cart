62db2b927525eca66ee65f71734fa38c417b0319
Colman Reilly <careilly@mac.com>
2016-08-07 20:23:50 +0100

    ok

--- app/helpers/interact_cart/cart_helper.rb
+++ app/helpers/interact_cart/cart_helper.rb
@@ -1,4 +1,10 @@
-q
+module InteractCart
+  module CartHelper
+    def ic_add_to_cart(product_id,product_price)
+      capture_haml do
+        haml_tag :a,"Add to cart",href: interact_cart.add_to_cart_path(product_id,price: product_price),class: "button"
+      end
+    end
     def ic_display_cart
       render partial: "interact_cart/cart/ic_display_cart", locals: { cart: @ic_cart }
     end
